96213e32de184d9bd1223c1a9c1a0a54043d912a
Yawei <adamiakjawel@gmail.com>
2018-05-22 22:26:10 +0200

    overhauled views, added an universal feed view, changed homepage temporarily

--- newsfeed/views.py
+++ newsfeed/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.base import TemplateView
 from .models import NewsPiece
-from django.http import HttpResponse
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class HomepageView(LoginRequiredMixin, ListView):
+class HomepageView(TemplateView):
+    template_name = 'home.html'
+
+
+class GenericFilteredView(LoginRequiredMixin, ListView):
     model = NewsPiece
     context_object_name = 'newspieces'
-    template_name = 'home.html'
+    template_name = 'feed.html'
     login_url = 'login'
+    filter_kwargs = {}
 
     def get_queryset(self):
-        return NewsPiece.objects.order_by('-publish_date')
-
-
-def test2(request):
-    return render(request, 'reddit_feed.html',)
+        try:
+            return NewsPiece.objects.filter(**self.filter_kwargs).order_by('-publish_date')
+        except TypeError:
+            return NewsPiece.objects.order_by('-publish_date')
