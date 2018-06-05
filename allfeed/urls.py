87887f57a0a82d7744fb672355ccab549ca55298
Yawei <adamiakjawel@gmail.com>
2018-06-05 12:22:02 +0200

    changed urls to not contain users username as it was not needed

--- allfeed/urls.py
+++ allfeed/urls.py
@@ -8,10 +8,10 @@ from accounts import views as accounts_views
 
 urlpatterns = [
     url(r'^$', views.HomepageView.as_view(), name='home'),
-    url(r'^(?P<username>[\w.@+-]+)/today/$', views.GenericFilteredView.as_view(
+    url(r'^my-allfeed/today/$', views.GenericFilteredView.as_view(
         filter_kwargs={'publish_date__gte': datetime.utcnow()-timedelta(days=1)}
     ), name='today'),
-    url(r'^(?P<username>[\w.@+-]+)/subscriptions/', accounts_views.SubscriptionView.as_view(),
+    url(r'^my-allfeed/subscriptions/', accounts_views.subscribe,
         name='subscriptions'),
     url(r'^signup/$', accounts_views.signup, name='signup'),
     url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
