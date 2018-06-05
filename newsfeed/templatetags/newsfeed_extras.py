8459fbd47258a5347da4d2d71b1dc0964e343e0d
Yawei <adamiakjawel@gmail.com>
2018-06-05 12:27:55 +0200

    removed unused template tags

--- newsfeed/templatetags/newsfeed_extras.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from ..models import NewsPiece
-
-register = template.Library()
-
-
-@register.simple_tag
-def filtered_querysets(*args):
-    return NewsPiece.site_filtered_queryset(args)
