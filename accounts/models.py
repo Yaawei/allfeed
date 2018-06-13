72c8d2d35cbc756a2724886a7f93975d6b005e47
Yawei <adamiakjawel@gmail.com>
2018-06-13 17:55:35 +0200

    add LikedEntry model

---
+++ accounts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+from newsfeed.models import NewsPiece
+
+
+class LikedEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    liked_entry = models.ForeignKey(NewsPiece, on_delete=models.CASCADE)
+
+
