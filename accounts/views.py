8a964cda848b8c0164969ea41f81f41678a79ac1
Yawei <adamiakjawel@gmail.com>
2018-06-13 17:49:04 +0200

    improve subscribe view

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import login
 from .forms import SignUpForm, FeedSubscriptionsForm
 from django.shortcuts import render, redirect
-from newsfeed.models import UserFeedChoices
+from newsfeed.models import UserFeedChoice, RssUrl
 
 
 def signup(request):
@@ -18,14 +18,23 @@ def signup(request):
 
 def subscribe(request):
     user = request.user
+    initial_qs = RssUrl.objects.filter(userfeedchoice__user=user)
     if request.method == 'POST':
         form = FeedSubscriptionsForm(request.POST)
         if form.is_valid():
             subscriptions = form.cleaned_data.get('subscription')
-            if subscriptions:
-                for query in subscriptions:
-                    UserFeedChoices.objects.get_or_create(user=user, rss_url=query)
-                return redirect('today')
-    else:
-        form = FeedSubscriptionsForm()
+            queries_to_delete = UserFeedChoice.objects.filter(
+                user=user,
+                rss_url__in=initial_qs.difference(subscriptions).values('id')
+            )
+            queries_to_add = subscriptions.difference(initial_qs)
+            for query in queries_to_add:
+                UserFeedChoice.objects.create(user=user, rss_url=query)
+            queries_to_delete.delete()
+            return redirect('subscriptions')
+    elif request.method == 'GET':
+        form = FeedSubscriptionsForm(
+            initial={'subscription': initial_qs}
+        )
     return render(request, 'subscription_form.html', {'form': form})
+
